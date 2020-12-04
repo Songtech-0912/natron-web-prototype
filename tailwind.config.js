0ddb0a30bca2b7f48b4eceb70041220b6e4b449d
J7314b <dixinlily@hotmail.com>
2020-12-04 19:18:20 +0800

    Implemented color palette for website theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,24 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        'light-blue': colors.lightBlue,
-        cyan: colors.cyan,
+    colors: {
+      slate: {
+        light: '#1a1e24', // most of background
+        DEFAULT: '#14171c', // footer only
       },
-    },
-  },
-  variants: {},
-  plugins: [],
+      red: {
+        DEFAULT: '#d94755', // accent
+      },
+      grey: {
+        dark: '#686e74', // primary dark
+        DEFAULT: '#bcc0c2', // primary text
+        light: '#fbfcfc', // primary bold
+      }
+    }
+  }
+  // variants: {}
+  // plugins: []
 }
 
-// These are the actual colors
-// substitute the original colors with these tomorrow
-// colors: {
-//       slate: {     
-//        light: '#1a1e24', // most of background
-//        DEFAULT: '14171c', // footer only
-//      },
-//      red: {
-//        DEFAULT: '#d94755', // accent
-//      },
-//      grey: {
-//        dark: '686e74', // primary dark
-//        DEFAULT: 'bcc0c2', // primary text
-//        light: '#fbfcfc', // primary bold
-// Use both z-index and box shadows for the gallery on the hero page
-//      }
-//    }
+
+
