1b3591494146aa398ada2d1531f1476c85a6bbe5
J7314b <dixinlily@hotmail.com>
2020-12-05 06:44:28 +0800

    Created automatic build script for gh-pages

---
+++ docs/build.sh
@@ -0,0 +1,7 @@
+# This takes the source css (the 'oldstyle.css')
+# and turns it into one clean index.css file
+# using the tailwind cli
+
+# BEGIN COMMANDS
+
+npx tailwind build styles/oldstyle.css -o styles/index.css
